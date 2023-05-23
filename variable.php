b0d8a4d0bb5cec9276ac0daa04123a098e063c72
takahashi <kinkin2203@gmail.com>
2023-05-23 14:17:32 +0900

    適当なコメント文

---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php
+$greeting = "こんばんは";
+echo $greeting;
+
+$price = 1000;
+echo $price;
+
+$a = 175.25;
+echo "<pre>";
+echo "<pre>";
+var_dump($price);//str
+var_dump(true); //論理値
+var_dump( 1 > 2 );
+var_dump( $greeting < "こんにちは" );
